26ba8d09de3a4cc52135802e70fc5603d9ed3e22
MinionStar <LouisLee214@gmail.com>
2022-08-06 09:41:17 -0400

    update globe image

--- sections/home-page/main-section/NoBodyWins.js
+++ sections/home-page/main-section/NoBodyWins.js
@@ -29,7 +29,7 @@ const NoBodyWins = ({}) => {
             data-aos="fade-bottom"
             data-aos-delay={200}
             data-aos-duration="500"
-            src="/images/globe.png"
+            src="/images/globe.jpg"
             alt="diamond-2"
             width={345}
             height={345}
